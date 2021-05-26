d6440d0cd9fc3dcbb2a3c1087857cf4d764c5dfc
njokuifeanyigerald <brainboyrichmond@gmail.com>
2021-05-26 10:02:32 +0100

    debug to false

--- UrlShortener/settings.py
+++ UrlShortener/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$srpga3jkfsq&s8jfmtjzpm&bcl_5oya_j@!c4o12_p4x*pvjf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
