87d7a654f9a58d8e6b13786f41d4b6f5afb9f933
njokuifeanyigerald <brainboyrichmond@gmail.com>
2021-07-06 10:52:22 +0100

    debug to false

--- UrlShortener/settings.py
+++ UrlShortener/settings.py
@@ -137,8 +137,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/images')
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
